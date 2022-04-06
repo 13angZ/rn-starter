173b6259f88054c1ff34002f668e3b9d0295b21d
13angz <6060503007@pnu.ac.th>
2022-04-06 09:29:33 +0700

    update color counter

--- src/screens/ColorCounterScreen.js
+++ src/screens/ColorCounterScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import ColorCounter from '../components/ColorCounter';
 
 function ColorCounterScreen() {
     const [colors, setColors] = React.useState({
@@ -11,9 +12,9 @@ function ColorCounterScreen() {
 
     return (
         <View>
-            <ColorCounter color/>
-            <ColorCounter color/>
-            <ColorCounter color/>
+            <ColorCounter color="Red" setColors={setColors}/>
+            <ColorCounter color="Green" setColors={setColors}/>
+            <ColorCounter color="Blue" setColors={setColors}/>
             <View
                 style={{...styles.innerViewStyle, backgroundColor: `rgb(${colors.Red}, ${colors.Green}, ${colors.Blue})`}}
             >
@@ -26,7 +27,9 @@ function ColorCounterScreen() {
 const styles = StyleSheet.create({
     innerViewStyle: {
         borderRadius: 12,
-        marginVertical: 12
+        marginVertical: 12,
+        width: 100,
+        height: 100
     }
 })
 
